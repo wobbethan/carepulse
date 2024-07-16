2ea53b9eb4bf4228a63729cca1bf76c278b66213
wobbethan <wobbethan@gmail.com>
2024-07-16 13:40:20 -0400

    more package downloads

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="">Home</h1>
+      <h1 className="text-white text-3xl">Home</h1>
     </div>
   );
 }
